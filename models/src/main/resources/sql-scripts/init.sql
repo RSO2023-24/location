5385875afbce560c3bc4cd81e376798018edd369
lucashj <lh3973@student.uni-lj.si>
2024-01-15 19:53:58 +0100

    Fixed initsql maybe

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,20 +1,7 @@
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Lipa', 'Restavracija', TRUE, TRUE, FALSE, 47.0516, 15.503);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Hrast', 'Brunarica', FALSE, TRUE, TRUE, 46.043339, 14.504097);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Kamin', 'Koca', TRUE, FALSE, TRUE, 46.048353, 14.499680);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Sedlo', 'Koca', FALSE, FALSE, FALSE, 46.050599, 14.503780);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Kovač', 'Hotel', TRUE, TRUE, TRUE, 46.051675, 14.510467);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Smreka', 'Restavracija', FALSE, TRUE, FALSE, 46.051075, 14.510487);
-
-INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) 
-VALUES ('Medved', 'Koca', TRUE, FALSE, TRUE, 46.0516, 14.503);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Lipa', 'Restavracija', TRUE, TRUE, FALSE, 47.0516, 15.503);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Hrast', 'Brunarica', FALSE, TRUE, TRUE, 46.043339, 14.504097);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Kamin', 'Koca', TRUE, FALSE, TRUE, 46.048353, 14.499680);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Sedlo', 'Koca', FALSE, FALSE, FALSE, 46.050599, 14.503780);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Kovač', 'Hotel', TRUE, TRUE, TRUE, 46.051675, 14.510467);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Smreka', 'Restavracija', FALSE, TRUE, FALSE, 46.051075, 14.510487);
+INSERT INTO image_metadata (name, description, toilets, food, sleep, longtitude, latitude) VALUES ('Medved', 'Koca', TRUE, FALSE, TRUE, 46.0516, 14.503);
